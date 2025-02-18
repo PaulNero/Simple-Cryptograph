44cc0f2f4666b56d5bb38887eee99396ea16680c
PaulNero <58290662+PaulNero@users.noreply.github.com>
2025-02-18 06:52:33 +0300

    trying to set up work with invs via github actions

--- bot.py
+++ bot.py
@@ -6,8 +6,11 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
+from dotenv import load_dotenv
 import os
 import base64
+
+load_dotenv()
 
 API_TOKEN = os.getenv("TOKEN")
 
